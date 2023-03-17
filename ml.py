9ed1078d7fa025b2d147aea47e3811cc8b3319f6
Sathvik-Malgikar <Sathvik.malgikar@gmail.com>
2023-03-17 13:48:20 +0530

    Create ml.py

---
+++ ml.py
@@ -0,0 +1,19 @@
+import cv2
+
+vid = cv2.VideoCapture(1)
+
+
+while(True):
+    
+    ret, frame = vid.read()
+    
+    cv2.imshow("Watch-Yantra",frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+    
+vid.release()
+
+cv2.destroyAllWindows()
+exit()
