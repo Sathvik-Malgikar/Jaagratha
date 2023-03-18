56c915491056faf924d344460245525bfaed5c16
Sathvik-Malgikar <Sathvik.malgikar@gmail.com>
2023-03-19 03:09:55 +0530

    Update whole.py

--- backend/whole.py
+++ backend/whole.py
@@ -9,14 +9,19 @@ import time
 
 def GRAND(msg,location,num,email):
     
+    print(f"now calling {num[0]}")
     primarynum= num[0]
+    print("done")
    
     
     say(f"{msg} ", primarynum)
     for curnum,curmail in zip(num,email):
-        
+        print(f"now mailing {curmail}")
         mail(f"{msg} {location}",email=curmail)
+
+        print("done")
         smsfoo(msg=msg,num=curnum,loc=location)
+        print("done")
         time.sleep(1)
     
 # GRAND("Medium level security is raised in Dwarakanagar. Get Moving! ","https://goo.gl/maps/ZcTaE9kFN5GL1CU67","7483064938","hemabhushanr3@gmail.com")
