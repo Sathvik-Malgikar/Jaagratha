b5a434fc20340573bd2a9ee8e8538036c5b8b35e
Pranav Ambiga <pranav.ambig123@gmail.com>
2023-03-18 15:32:35 +0530

    auth completed

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,10 @@
-from flask import Flask, Response
-
+from flask import Flask, Response, request
 from flask_cors import CORS
-
 import cv2
 from time import sleep
 import socket
+import json
+import pymongo
 
 
 vid1 = cv2.VideoCapture("http://192.168.137.128:4747/video?640x480")
@@ -23,16 +23,26 @@ def gen_frames():
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/auth')
+def auth(data):
+    import pymongo
+    client = pymongo.MongoClient("mongodb://localhost:27017")
+    auth_db = client["Jaagrutha"]
+    user_pass = auth_db["Auth"]
+    queryResult = user_pass.find(data)
+    for x in queryResult:
+        return "1"
+    return "0"
+
+@app.route('/auth', methods=["GET", "POST"])
 def home():
-    return 'Hello, World!'
+    print(json.loads(request.data))
+    return auth(json.loads(request.data))
+
 
 @app.route('/feed', methods=["POST", "GET"])
 def videofeed1():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
     
-
-
 
 if __name__ == '__main__':
     app.run()
