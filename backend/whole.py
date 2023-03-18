640938fbcc24e06f27d0ccec6879300a78504a73
Sathvik-Malgikar <Sathvik.malgikar@gmail.com>
2023-03-18 23:50:27 +0530

    Update whole.py

--- backend/whole.py
+++ backend/whole.py
@@ -6,10 +6,10 @@ from sms import smsfoo
 
 # pywhatkit.sendwhatmsg("+916360709909", "Hello pywhat",8+12,2)
 
-def GRAND(msg,location):
-    say(f"{msg} ", "7483064938")
-    mail(f"{msg} {location}","hemabhushanr3@gmail.com")
-    smsfoo(msg=msg,num="7483064938")
+def GRAND(msg,location,num,email):
+    say(f"{msg} ", num)
+    mail(f"{msg} {location}",email=email)
+    smsfoo(msg=msg,num=num)
     
-GRAND("Medium level security is raised in Dwarakanagar. Get Moving! ","https://goo.gl/maps/ZcTaE9kFN5GL1CU67")
+# GRAND("Medium level security is raised in Dwarakanagar. Get Moving! ","https://goo.gl/maps/ZcTaE9kFN5GL1CU67","7483064938","hemabhushanr3@gmail.com")
     
