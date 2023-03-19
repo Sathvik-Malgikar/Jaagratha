b21fc00e77869936b2bc8f4235d5d2ef287803e2
Sathvik-Malgikar <Sathvik.malgikar@gmail.com>
2023-03-19 06:11:30 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -26,8 +26,7 @@ vidFeeds = []
 crimeDetector = keras.models.load_model("crime_detect_modelH5.h5")
 
 
-
-## face recogniser specific stuff
+# face recogniser specific stuff
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read('trainer.yml')
 cascadePath = "haarcascade_frontalface_default.xml"
@@ -39,67 +38,73 @@ minW = 0.1*vid1.get(3)
 minH = 0.1*vid1.get(4)
 
 buffer_dir = r"C:\Users\prana\Desktop\Workspaces\Hack Attack\Jaagratha\backend\buffer\Explosion"
-crimeDetector = keras.models.load_model(r'C:\Users\prana\Desktop\Workspaces\Hack Attack\Jaagratha\backend\test\crime_detect_modelH5.h5')
+crimeDetector = keras.models.load_model(
+    r'C:\Users\prana\Desktop\Workspaces\Hack Attack\Jaagratha\backend\test\crime_detect_modelH5.h5')
 preprocess_fun = tf.keras.applications.densenet.preprocess_input
 final_gen = ImageDataGenerator(horizontal_flip=True,
-                                   width_shift_range=0.1,
-                                   height_shift_range=0.05,
-                                   rescale = 1./255,
-                                   preprocessing_function=preprocess_fun
-                                  )
+                               width_shift_range=0.1,
+                               height_shift_range=0.05,
+                               rescale=1./255,
+                               preprocessing_function=preprocess_fun
+                               )
+
 
 async def idk(a, b, c, d):
     print("figure out")
     loop = asyncio.get_running_loop()
+
     def lamb():
-        return idk(a,b,c,d)
-    result = await loop.run_in_executor(None,lamb) 
-    
+        return idk(a, b, c, d)
+    result = await loop.run_in_executor(None, lamb)
+
     print("Result obtained!!! S U C C E S S")
     print(result)
-    
+
     # await whole.GRAND(a, b, c, d)
-    
+
 
 def genVidFeed(vid):
     prob = 1
     count = 1
+
     def gen_frames():
         nonlocal count, prob
-        timer=time.time()
+        timer = time.time()
         while True:
             count += 1
             success, frame = vid.read()  # read the camera frame
             # cv2.imwrite("temp")
             if (prob == 0):
-                final_gen2 = final_gen.flow_from_directory(directory = buffer_dir,
-                                                    target_size = (64, 64),
-                                                    batch_size = 1,
-                                                    shuffle  = True , 
-                                                    color_mode = "rgb",
-                                                    class_mode = "categorical",
-                                                    seed = 10,
-                                                    classes = ['Abuse','Arrest','Arson','Assault','Burglary','Explosion','Fighting',"Normal",'RoadAccidents','Robbery','Shooting','Shoplifting','Stealing','Vandalism']
-                                                   )
+                final_gen2 = final_gen.flow_from_directory(directory=buffer_dir,
+                                                           target_size=(
+                                                               64, 64),
+                                                           batch_size=1,
+                                                           shuffle=True,
+                                                           color_mode="rgb",
+                                                           class_mode="categorical",
+                                                           seed=10,
+                                                           classes=['Abuse', 'Arrest', 'Arson', 'Assault', 'Burglary', 'Explosion', 'Fighting',
+                                                                    "Normal", 'RoadAccidents', 'Robbery', 'Shooting', 'Shoplifting', 'Stealing', 'Vandalism']
+                                                           )
 
-                frame2 = cv2.resize(frame, (64,64))
+                frame2 = cv2.resize(frame, (64, 64))
                 frame2 = tf.expand_dims(frame2, axis=0)
                 pred_val = crimeDetector(frame2)
                 pred_val = tf.get_static_value(pred_val)[0]
 
                 maxval = max(pred_val)
-                #flipped for testing
+                # flipped for testing
                 if maxval == pred_val[7]:
                     prob = 0
                 else:
                     prob = 1
-            
+
             # frame = cv2.resize(frame, (64, 64))
-            
+
             if not success:
                 break
             else:
-                #face recog start
+                # face recog start
                 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
                 faces = faceCascade.detectMultiScale(
@@ -110,12 +115,13 @@ def genVidFeed(vid):
                 )
                 # my modifications
                 if faces != []:
-                    if(time.time()>=timer):
-                        timer=timer+50
-                        print("grand called in face detection",timer)
-                        cv2.imwrite("suspect.png",frame)
-                        task = asyncio.create_task(idk("Medium level security is raised in Dwarakanagar. Get Moving! ","https://goo.gl/maps/ZcTaE9kFN5GL1CU67",["7483064938","7019486115","9380900636 "],["hemabhushanr3@gmail.com","sathvik.malgikar@gmail.com","rashmipr9496@gmail.com"])) 
-                        
+                    if (time.time() >= timer):
+                        timer = timer+50
+                        print("grand called in face detection", timer)
+                        cv2.imwrite("suspect.png", frame)
+                        task = asyncio.create_task(idk("Medium level security is raised in Dwarakanagar. Get Moving! ", "https://goo.gl/maps/ZcTaE9kFN5GL1CU67", [
+                                                   "7483064938", "7019486115", "9380900636 "], ["hemabhushanr3@gmail.com", "sathvik.malgikar@gmail.com", "rashmipr9496@gmail.com"]))
+
                 idTemp = []
                 for (x, y, w, h) in faces:
                     cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
@@ -151,12 +157,11 @@ def genVidFeed(vid):
                         (255, 255, 0),
                         1
                     )
-               
-                
+
                 # if prob:
                 #     print("prob")
                 #     cv2.putText(frame, "Crime Detected", (200, 40), font, 1, (0, 0, 255), 1)
-                
+
                 # if(time.time()>=timer):
                 #     print("grand called")
                 #     cv2.imwrite("suspect.png",frame)
@@ -164,16 +169,17 @@ def genVidFeed(vid):
                 #     timer=timer+50
 
                 ret, buffer = cv2.imencode('.jpg', frame)
-                
-                
+
                 frame = buffer.tobytes()
                 yield (b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') 
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
     return gen_frames
+
 
 def init():
     for vid in vidStreams:
         vidFeeds.append(genVidFeed(vid))
+
 
 init()
 
@@ -187,6 +193,7 @@ def auth(data):
         return "1"
     return "0"
 
+
 @app.route('/auth', methods=["GET", "POST"])
 def home():
     return auth(json.loads(request.data))
@@ -196,8 +203,7 @@ def home():
 def videofeed():
     id = int(request.args.get("id"))
     return Response(vidFeeds[id](), mimetype='multipart/x-mixed-replace; boundary=frame')
-    
-    
+
 
 if __name__ == '__main__':
     app.run()
